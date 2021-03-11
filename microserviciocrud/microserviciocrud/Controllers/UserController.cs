4471283d65393d5f4b4d4eccc1c757fb13acde51
Marianbrero <79025036+Marianbrero@users.noreply.github.com>
2021-03-11 15:13:54 -0600

    Add files via upload

---
+++ microserviciocrud/microserviciocrud/Controllers/UserController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using microserviciocrud.Database;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace microserviciocrud.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        // GET: api/<Controller>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            DB cn = new DB();
+            return new string[] { cn.MostrarInfo("Select * from Usuarios", 4) };
+        
+        }
+
+        // GET api/<Controller>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<Controller>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<Controller>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<Controller>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ microserviciocrud/microserviciocrud/Database/DB.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace microserviciocrud.Database
+{
+    public class DB
+    {
+        SqlConnection conexionn = new SqlConnection("Data Source=MARIAN\\MSSQLSERVER2;Initial Catalog=userm;User ID=sa;Password=3483440");
+    
+        public string MostrarInfo(string funcion, int cant)
+        {
+            string resultado = "";
+            conexionn.Open();
+            SqlCommand comando = new SqlCommand(funcion, conexionn);
+            SqlDataReader lector = comando.ExecuteReader();
+            while (lector.Read())
+            {
+                for (int i = 0; i < cant; i++)
+                {
+                    resultado += lector.GetValue(i).ToString() + " ";
+                }
+            }
+            conexionn.Close();
+            return resultado;
+        }
+    }
+}
